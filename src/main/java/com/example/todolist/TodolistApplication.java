45351be232c292f455c9b02c8e872a65091ed897
YuriyPanchenko <yuriy.panchenko.2000@gmail.com>
2019-11-01 00:46:38 +0200

    fix cors

--- src/main/java/com/example/todolist/TodolistApplication.java
+++ src/main/java/com/example/todolist/TodolistApplication.java
@@ -2,9 +2,28 @@ package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodolistApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/*")
+                        .allowedOrigins(
+                                "https://todolist-backend-springboot.herokuapp.com",
+                                "https://liquidcore7.localtunnel.me"
+                        );
+            }
+        };
+        return webMvcConfigurer;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TodolistApplication.class, args);
